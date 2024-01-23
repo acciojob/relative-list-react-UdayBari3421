8a91fa8cff57ef7eef39342d0f7787c84998f501
UdayBari3421 <udaybari2003@gmail.com>
2024-01-23 15:29:42 +0530

    submitting code-Tue Jan 23 2024 15:29:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [arr, setArr] = useState([]);
+
+  function handle(e) {
+    e.preventDefault();
+    setArr([...arr, name]);
+    console.log(arr.length);
+  }
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <form onSubmit={handle}>
+        <input
+          type="text"
+          name="relativeName"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ol key="relativeList">
+        {arr.length > 0 &&
+          arr.map((item, index) => {
+            return <li key={"relativeListItem" + index}>{item}</li>;
+          })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
